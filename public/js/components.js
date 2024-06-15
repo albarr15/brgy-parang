f547d56504eb1537c84b11f471bf8b95f643d30f
Clarissa Albarracin <cmalbarracin15@gmail.com>
2024-06-15 20:46:57 +0800

    Added case status indicators

--- public/js/components.js
+++ public/js/components.js
@@ -6,4 +6,26 @@ document.addEventListener("DOMContentLoaded", function() {
             this.classList.toggle("clicked");
         });
     });
+
+    var status_btns = document.getElementsByClassName("status");
+
+    Array.from(status_btns).forEach(function(status_btns) {
+        status_btns.addEventListener("click", function() {
+            var currentState = this.classList[1];
+            var img = this.querySelector('img');
+            var text = this.querySelector('span');
+
+            if (currentState == "resolved") {
+                this.classList.remove('resolved');
+                this.classList.add('ongoing');
+                img.src="../public/images/ongoing-circle.png"
+                text.textContent = "Ongoing";
+            } else {
+                this.classList.remove('ongoing');
+                this.classList.add('resolved');
+                img.src="../public/images/resolved-circle.png"
+                text.textContent = "Resolved";
+            }
+        });
+    });
 });
