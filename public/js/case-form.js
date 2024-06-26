4ef6354506a80ff8cb4dc8b99b1e44770d1a412f
Clarissa Albarracin <cmalbarracin15@gmail.com>
2024-06-26 17:40:15 +0800

    Autocapitalization enabled for all middle initial inputs

--- public/js/case-form.js
+++ public/js/case-form.js
@@ -1,7 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+
     var dateInput = document.querySelector("#dateInput");
 
-    // sets date max to current date in the format mm/dd/yyyy
-    dateInput.max = new Date().toLocaleDateString('fr-ca');
-    dateInput.min = "1970-01-01";
+    if (dateInput) {
+        // sets date max to current date in the format mm/dd/yyyy
+        dateInput.max = new Date().toLocaleDateString('fr-ca');
+
+        // sets date minimum to January 1, 1970
+        dateInput.min = "1970-01-01";
+    }
+
+    // autocapitalizes middle initial inputs
+    document.querySelectorAll('.middle-name-input').forEach(function(mid_initial) {
+        mid_initial.addEventListener('input', function() {
+            this.value = this.value.toUpperCase();
+        });
+    });
 });
