95e334bf1f872e1ee1716693fa62bf359f17d944
Althea <reina_garcia@dlsu.edu.ph>
2024-07-02 19:07:17 +0800

    Login feature implementation

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-// testing
-const { CertificateModel, UserModel, LuponCaseModel, TanodCaseModel } = require('./models/database/mongoose');
-
 const express = require('express');
-
 const app = express();
+const admin_loginRoutes = require('./routes/admin-loginRoutes');
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+//This is a new library called Body Parser. This system will parse the data
+//from its internal JSon system to make request messages simpler.
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+
 
 const handlebars = require('express-handlebars');
 app.set('view engine', 'hbs');
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }));
+
+app.get('/', function(req, res){
+    res.render('index',{
+        layout: 'layout',
+        title: 'Barangay Parang - Initial Login Page',
+        cssFile1: 'index',
+        cssFile2: null,
+        javascriptFile1: null,
+        javascriptFile2: null,
+    });
+});
+
+app.get('/index', function(req, res){
+    res.render('index',{
+        layout: 'layout',
+        title: 'Barangay Parang - Initial Login Page',
+        cssFile1: 'index',
+        cssFile2: null,
+        javascriptFile1: null,
+        javascriptFile2: null,
+    });
+});
+
+
+//login route
+app.use(admin_loginRoutes);
 
 // Start the server
 const port = process.env.PORT || 3000;
