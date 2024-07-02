9b5afedf3d6da5c748d9afd9f2d3e304ddded262
Althea <reina_garcia@dlsu.edu.ph>
2024-07-02 12:27:18 +0800

    Added mongoose.js for database integration. See the file for comments.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+// testing
+const { CertificateModel, UserModel, LuponCaseModel, TanodCaseModel } = require('./models/database/mongoose');
+
 const express = require('express');
 
 const app = express();
 
-var hbs = require('hbs')
-app.set('view engine','hbs');
+const handlebars = require('express-handlebars');
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, function(){
+    console.log('Listening at port '+ port);
+});
---
+++ models/database/mongoose.js
@@ -0,0 +1,257 @@
+/*
+    I followed the database design but removed the Person Schema and included it directly in the LuponCase and TanodCase schemas
+ */
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/BrgyParang_Database')
+  .then(() => {
+    console.log('Connection successful');
+  })
+  .catch((error) => {
+    console.error('Something went wrong', error);
+});
+
+// SCHEMA //
+const CertificateSchema = new mongoose.Schema ({
+    _id: {
+        type: String,
+        required: true
+    },
+    img : {
+        type: String,
+        required: true
+    },
+    name : {
+        type: String,
+        required: true
+    },
+    birthday : {
+        type: String,
+        required: true
+    },
+    address : {
+        type: String,
+        required: true
+    },
+    ctc_no : {
+        type: Number,
+        required: true
+    },
+    ctc_date_issued : {
+        type: String,
+        required: true
+    },
+    ctc_location : {
+        type: String,
+        required: true
+    },
+    cert_date_issued : {
+        type: String,
+        required: true
+    },
+    reason : {
+        type: String,
+        required: true
+    }
+});
+
+const UserSchema = new mongoose.Schema ({
+    _id: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    },
+});
+
+const LuponCase = new mongoose.Schema ({
+    _id: {
+        type: String,
+        required: true
+    },
+    CaseTitle: {
+        type: String,
+        required: true,
+    },
+    CaseType: {
+        type: String,
+        required: true,
+    },
+    Status: {
+        type: String,
+        required: true,
+    },
+    RespondentInfo: {
+        FirstName: {
+            type: String,
+            required: true,
+        },
+        MiddleInitial: {
+            type: String,
+            required: true,
+        },
+        LastName: {
+            type: String,
+            required: true,
+        },
+    },
+    ComplainerInfo: {
+        FirstName: {
+            type: String,
+            required: true,
+        },
+        MiddleInitial: {
+            type: String,
+            required: true,
+        },
+        LastName: {
+            type: String,
+            required: true,
+        },
+    },
+    MediationInfo: {
+        FirstName: {
+            type: String,
+            required: true,
+        },
+        MiddleInitial: {
+            type: String,
+            required: true,
+        },
+        LastName: {
+            type: String,
+            required: true,
+        },
+    },
+    ConciliationInfo: {
+        FirstName: {
+            type: String,
+            required: true,
+        },
+        MiddleInitial: {
+            type: String,
+            required: true,
+        },
+        LastName: {
+            type: String,
+            required: true,
+        },
+    },
+    Case: {
+        type: String,
+        required: true,
+    },
+});
+
+const TanodCase = new mongoose.Schema ({
+    _id: {
+        type: String,
+        required: true
+    },
+    EntryNo: {
+        type: Number,
+        required: true,
+    },
+    Date: {
+        type: String,
+        required: true,
+    },
+    Status: {
+        type: String,
+        required: true,
+    },
+    ReporteeInfo: {
+        FirstName: {
+            type: String,
+            required: true,
+        },
+        MiddleInitial: {
+            type: String,
+            required: true,
+        },
+        LastName: {
+            type: String,
+            required: true,
+        },
+        Address: {
+            type: String,
+            required: true
+        }
+    },
+    natureOfBlotter: {
+        type: String,
+        required: true
+    },
+    RespondentInfo: {
+        FirstName: {
+            type: String,
+            required: true,
+        },
+        MiddleInitial: {
+            type: String,
+            required: true,
+        },
+        LastName: {
+            type: String,
+            required: true,
+        },
+    },
+    DeskOfficerInfo: {
+        FirstName: {
+            type: String,
+            required: true,
+        },
+        MiddleInitial: {
+            type: String,
+            required: true,
+        },
+        LastName: {
+            type: String,
+            required: true,
+        },
+    },
+    WitnessInfo: {
+        FirstName: {
+            type: String,
+            required: true,
+        },
+        MiddleInitial: {
+            type: String,
+            required: true,
+        },
+        LastName: {
+            type: String,
+            required: true,
+        },
+    },
+    Location: {
+        type: String,
+        required: true
+    },
+});
+
+// MODELS //
+const CertificateModel  = mongoose.model("CertificateModel", CertificateSchema, "certificate");
+const UserModel         = mongoose.model("UserModel", UserSchema, "user");
+const LuponCaseModel    = mongoose.model("LuponCaseModel", LuponCase, "lupon_case");
+const TanodCaseModel    = mongoose.model("TanodCaseModel", TanodCase, "tanod_case");
+
+
+// EXPORTS //
+module.exports = {
+    CertificateModel,
+    UserModel,
+    LuponCaseModel,
+    TanodCaseModel
+};
