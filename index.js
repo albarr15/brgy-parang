d39dbb940f1f5c2119199699a606731dcb405d56
Althea <reina_garcia@dlsu.edu.ph>
2024-07-12 08:29:46 +0800

    additional changes

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
-// testing
-const { CertificateModel, UserModel, LuponCaseModel, TanodCaseModel } = require('./models/database/mongoose');
-
 const express = require('express');
 const app = express();
 
-const certificate_PrintingRoutes = require('./routes/certificate-printingRoutes');
 
-const handlebars = require('express-handlebars');
-app.set('view engine', 'hbs');
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs'
-}));
+
+const { registerHelpers } = require('./helpers/handlebarHelpers');
+const certificate_PrintingRoutes = require('./routes/certificate-printingRoutes');
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 const bodyParser = require('body-parser');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
-
-function errorFn(err){
-    console.log('Error fond. Please trace!');
-    console.error(err);
-}
-
-const { registerHelpers } = require('./helpers/handlebarHelpers');
-//helpers
-registerHelpers();
-
-// Start the server
-const mongoose = require('mongoose');
+const handlebars = require('express-handlebars');
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
 
 app.get('/', function(req, res){
     res.render('index',{
@@ -53,12 +43,24 @@ app.get('/index', function(req, res){
     });
 });
 
+//helpers
+registerHelpers();
+
+
+
+
+
+
+
+
+
 const controllers = ['employee-tanod-lupon-routes']; //ung mga get eme nasa controller
 for(var i=0; i<controllers.length; i++){
   const model = require('./controllers/'+controllers[i]);
   model.add(app);
 }
 
+//certofocate printing routes
 app.use(certificate_PrintingRoutes);
 
 const port = process.env.PORT || 3000;
